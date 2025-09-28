0dcb0da2944283229fbdf23d38e6df38620f7051
MagnumGOYB <sosbs@outlook.com>
2025-09-28 22:03:29 +0800

    ✨ feat: fix

--- mpx.config.js
+++ mpx.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   outputDir: `dist/${process.env.MPX_CURRENT_TARGET_MODE}`,
   pluginOptions: {
     mpx: {
       plugin: {
         srcMode: 'wx',
+        css: ['css', 'scss', 'sass'],
         hackResolveBuildDependencies: ({ files, resolveDependencies }) => {
           const path = require('path')
           const packageJSONPath = path.resolve('package.json')
@@ -13,13 +15,12 @@ module.exports = defineConfig({
             resolveDependencies.files.delete(packageJSONPath)
           }
         }
-      },
-      loader: {}
+      }
     }
-  },
+  }
   /**
    * 如果希望node_modules下的文件时对应的缓存可以失效，
    * 可以将configureWebpack.snap.managedPaths修改为 []
    */
-  configureWebpack(config) {}
+  // configureWebpack(config) {}
 })
